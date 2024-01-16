4ec425e98de2317e3dacb08573a327282d762b5e
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 09:41:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+let isModal=true;
+   function openModal(){
+    if(isModal){
+	let body =document.querySelector("body");
+
+		let overlay = document.createElement("div");
+		overlay.className = "overlay";
+		body.appendChild(overlay); 
+		
+	let htmlElem = `
+        <div class="modal">
+		 <h2>Modal Header</h2>
+		  <div class="body">
+			   <h2>modal body content</h2>
+			    <h3 onClick="closeModal()">✖</h3>
+		  </div>
+	 </div>
+	`
+	body.insertAdjacentHTML("beforeend",htmlElem);
+	
+	isModal=false
+}
+}
+function closeModal(){
+	let overlay  =document.querySelector(".overlay");
+	 let modal  = document.querySelector(".modal");
+
+	if(overlay){
+      overlay.remove();
+	}
+	if(!isModal){
+		isModal =true;
+	    modal.remove();
+	}
+	
+}
