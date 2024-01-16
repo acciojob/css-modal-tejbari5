6942a240e38f76c08202aa42571601567ca373a8
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-16 09:55:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
 //your JS code here. If required.
-let isModal=true;
-   function openModal(){
-    if(isModal){
-	let body =document.querySelector("body");
+// Get the modal and button
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModal");
 
-		let overlay = document.createElement("div");
-		overlay.className = "overlay";
-		body.appendChild(overlay); 
-		
-	let htmlElem = `
-        <div class="modal">
-		 <h2>Modal Header</h2>
-		  <div class="body">
-			   <h2>modal body content</h2>
-			    <h3 onClick="closeModal()">✖</h3>
-		  </div>
-	 </div>
-	`
-	body.insertAdjacentHTML("beforeend",htmlElem);
-	
-	isModal=false
-}
-}
-function closeModal(){
-	let overlay  =document.querySelector(".overlay");
-	 let modal  = document.querySelector(".modal");
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+};
 
-	if(overlay){
-      overlay.remove();
-	}
-	if(!isModal){
-		isModal =true;
-	    modal.remove();
-	}
-	
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        closeModal();
+    }
+};
+
+// Close the modal
+function closeModal() {
+    modal.style.display = "none";
 }
